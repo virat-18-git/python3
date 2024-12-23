6d24ea8aa18cdd2c0886f952d9545ba8a1c8e521
virat-18-git <ksprddy6@gmail.com>
2024-12-24 02:48:50 +0530

    loops,statements

---
+++ binary.py
@@ -0,0 +1,97 @@
+# int1=100
+# int2=100
+# print(id(int1))
+# print(id(int2))
+
+# str1="shiva"
+# str2="shiva"
+# print(id(str1))
+# print(id(str2))
+
+
+# assignment operators
+# print(456==456)
+# print(456!=456)
+# print(456>456)
+# print(456<456)
+# print(456>=456)
+# print(456<=456)
+
+
+# relational operators
+# print(456==456 and 789==789)
+# print(456==456 and 789!=789)
+# print(456!=456 and 789!=789)
+# print(False and 5)
+
+# print(456==456 or 789==789)
+# print(456==456 or 789!=789)
+# print(456!=456 or 789!=789)
+# print(True or 5)
+
+# print(5 or 10)
+# print(5 or -10)
+# print(-5 or 10)
+# print(0 or 10)
+
+# print(not 10==10)
+
+# number to binary conversion
+# print(bin(25))
+# print(oct(25))
+# print(hex(25))
+
+
+# bitwise operator
+# print(15 & 10)
+# print(15 | 10)
+# print(15 ^ 10) #xor symbol
+# print(15 >> 10)
+# print(15 << 10)
+# print(~15)
+
+
+# control statements
+# 1.conditional statements:else,if,else if
+# 2.loops:for,while
+# 3.jump statements:continue,break,pass
+
+# name="virat"
+# name=="yuvraj"
+# if name=="dhoni":
+#     print("century")
+# elif name=="yuvraj":
+#     print("G.O.A.T")
+# elif name=="dhoni":
+#     print("cool cap")
+# else:
+#    print("half century")
+
+# num == 12
+# if sum > 0:
+#     print("greater")
+# else:
+#     print("less than")
+
+
+# loops
+# for i in range(25,50):
+#     print(i)
+# if i%2 == 0:
+#     print(i,"even")
+# else:
+#     print(i,"odd")
+
+list=[1,8,6,9,7]
+for i in list:
+    print(i)
+    if i==9:
+        print("found")
+    else:
+        print("not found")
+
+
+
+
+
+
